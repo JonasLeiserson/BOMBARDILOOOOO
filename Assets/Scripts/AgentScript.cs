56dbf81ad82b73f61b11b10887b2594a682fa501
Jonas "El  pipa" LECHERSON <49516747@est.ort.edu.ar>
2025-09-22 10:41:07 -0300

    Initial commit

---
+++ Assets/Scripts/AgentScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentScript : MonoBehaviour
+{
+    NavMeshAgent agent;
+    [SerializeField] Transform targetTR;
+    [SerializeField] Animator anim;
+    [SerializeField] float velocity;
+    [SerializeField] Transform[] Puntos;
+    public bool patrullando = true;
+    private int currentPatrolPoint = 0;
+    public static AgentScript Instance;
+    public GameObject jugador;
+
+    private void Awake()
+    {
+        Instance = this;
+        agent = GetComponent<NavMeshAgent>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (patrullando)
+        {
+            if(!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                currentPatrolPoint = (currentPatrolPoint + 1) % Puntos.Length;
+                agent.SetDestination(Puntos[currentPatrolPoint].position);
+            }
+        }
+            else
+            {
+                agent.SetDestination(jugador.transform.position);
+            }
+            velocity = agent.velocity.magnitude;
+            anim.SetFloat("Speed",velocity);
+    }
+}
---
+++ Assets/raycast.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycast : MonoBehaviour
+{
+    float RaycastDistancia = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward * RaycastDistancia, Color.red);
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hitInfo, RaycastDistancia))
+        {
+            if (hitInfo.collider.gameObject.tag == "Player")
+            {
+                AgentScript.Instance.patrullando = false;
+            }
+        }
+    }
+}
